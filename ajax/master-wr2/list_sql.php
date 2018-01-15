7928c60cd5856261ebf5ef0f28bcd15cd0938406
Ian <ian.hbs62@gmail.com>
2018-01-16 00:01:16 +0800

    master-wr2.list_sql [memperbaiki sql]

--- ajax/master-wr2/list_sql.php
+++ ajax/master-wr2/list_sql.php
@@ -1,4 +1,4 @@
 <?php
 	$list_sql = "SELECT npwrd,no_registrasi,nm_wp_wr,alamat_wp_wr,kelurahan,kecamatan,kota,tgl_pendaftaran 
-				 FROM public.app_reg_wr WHERE(jenis_retribusi='2')";
+				 FROM public.app_reg_wr WHERE(tipe_retribusi='2')";
 ?>
